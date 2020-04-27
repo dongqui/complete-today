fec81b5e06f2a5d7476eec7fee59221f992380c2
dongqui <kimwinner90@gmail.com>
2020-04-27 15:20:14 +0900

    init todos reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,12 +1,28 @@
+import { action } from './helper';
+
 const ADD = 'todos/ADD';
 const DELETE = 'todos/DELETE';
+const DONE = 'todos/DONE';
+
+export const addTodo = action(ADD);
+export const deleteTodo = action(DELETE);
+export const doneTodo = action(DONE);
 
 const initialState = {
   todos: [],
+  dones: []
 }
 
 const reducer = (state=initialState, action) => {
-  switch (action.type) {
+  const { type, payload } = action;
+  switch (type) {
+    case ADD: 
+      return { ...state, todos: [...state.todos, payload] }
+    case DELETE:
+      return {...state, todos: state.todos.filter(v => v !== payload)}
+    case DONE:
+      return {...state, todos: state.todos.filter(v => v !== payload), dones: [...state.dones, payload]}
+
   }
   return state;
 }
