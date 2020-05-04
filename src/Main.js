e1a66279ef515890d4f8e6c846a552b81ac6488b
dongqui <kimwinner90@gmail.com>
2020-05-04 17:37:28 +0900

    init data load

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { Provider } from 'react-redux'
+import React, { useEffect } from 'react';
+import { Provider, useDispatch } from 'react-redux'
 import { createStore } from 'redux';
 import reducer from './reducers/index';
 
-import { Platform, StatusBar, StyleSheet, View, Text } from 'react-native';
-import { SplashScreen } from 'expo';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+
+import TodoList from './components/TodoList/TodoList';
+
+import { initTodoThunk } from './reducers/todos'
 
 const store = createStore(reducer);
 
 export default function Main(props) {
-  const [ isLoadingComplete, setLoadingComplete ] = useState(false);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
- 
-  }, []);
+    dispatch(initTodoThunk());
+  }, [dispatch]);
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
-    return (
-      <Provider store={store}>
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        
-        </View>
-      </Provider>
-    );
-  }
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <TodoList />
+      </View>
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
