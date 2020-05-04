84f25021a8495f0dcd8fc8679bb48d94992d3e53
dongqui <kimwinner90@gmail.com>
2020-05-04 17:55:08 +0900

    wraping App

--- App.js
+++ App.js
@@ -1,2 +1,12 @@
-import Main from './src/Main.js'
-export default Main
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import Main from './src/Main.js';
+import reducer from './src/reducers/index';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+const App = () => <Provider store={store}><Main/></Provider>
+
+export default App
--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,11 @@
 import React, { useEffect } from 'react';
-import { Provider, useDispatch } from 'react-redux'
-import { createStore } from 'redux';
-import reducer from './reducers/index';
+import { useDispatch } from 'react-redux'
 
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 
 import TodoList from './components/TodoList/TodoList';
 
 import { initTodoThunk } from './reducers/todos'
-
-const store = createStore(reducer);
 
 export default function Main(props) {
 
@@ -20,12 +16,10 @@ export default function Main(props) {
   }, [dispatch]);
 
   return (
-    <Provider store={store}>
       <View style={styles.container}>
         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
         <TodoList />
       </View>
-    </Provider>
   );
 }
 
