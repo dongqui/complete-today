1298d11e91606175acd972197d6c9f1fcd8194f5
Unknown <kimwinner90@gmail.com>
2020-05-12 18:28:05 +0900

    함수 이름 변경, id 값 increment

--- src/localStorage/index.js
+++ src/localStorage/index.js
@@ -1,14 +1,31 @@
 import { AsyncStorage } from 'react-native';
 
-export const setData = async (key, value) => {
+export const insertTodo = async (key, value) => {
   try {
-    return await AsyncStorage.setItem(key, value);
+    let lastId = await AsyncStorage.getItem('lastId');
+    lastId = lastId !== null ? +(lastId) + 1 : '0';
+    value['id'] = lastId;
+    const todosStr = await AsyncStorage.getItem(key);
+    const todos = todosStr !== null ? [...JSON.parse(todosStr), value] : [value];
+    await AsyncStorage.multiSet([[key, JSON.stringify(todos)], ['lastId', lastId + '']]);
+    return await AsyncStorage.getItem(key);
   } catch (e) {
     return e;
   }
 }
 
-export const getData = async (key) => {
+export const updateTodo = async (key, value) => {
+  try {
+    const todosStr = await AsyncStorage.getItem(key);
+    const todos = [...JSON.parse(todosStr).filter(v => v.id !== value.id), value]
+    await AsyncStorage.setItem(key, JSON.stringify(todos));
+    return await AsyncStorage.getItem(key);
+  } catch (e) {
+    return e;
+  }
+}
+
+export const selectTodo = async (key) => {
   try {
     return await AsyncStorage.getItem(key);
   } catch(e) {
