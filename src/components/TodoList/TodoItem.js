877aec308b5f1153a20dd1d452b864514f3248f9
Unknown <kimwinner90@gmail.com>
2020-05-12 18:25:35 +0900

    이미지 추가 / 옵션 ui

--- src/components/TodoList/TodoItem.js
+++ src/components/TodoList/TodoItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Text, View, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 import styles from './TodoStyles';
 
@@ -19,6 +20,10 @@ export default function TodoItem({ todo }) {
     dispatch(putTodoThunk({...todo, activate: false}))
   }
 
+  const onPressEdit = () => {
+
+  }
+
   const press = () => {
     if (todo.id === pressed) {
       dispatch(pressTodo(null))
@@ -30,14 +35,28 @@ export default function TodoItem({ todo }) {
   return (
     <TouchableOpacity onPress={press}>
       <View style={styles.itemContainer}>
+        <AntDesign onPress={onPressDone} name="smileo" size={24} color="black" />
         <Text style={styles.todoItem}>{todo.content}</Text>
       </View>
 
       {pressed === todo.id && 
       <View style={styles.optionList}>
-        <Text onPress={onPressDone} style={styles.optionItem}>완료</Text>
-        <Text style={styles.optionItem}>수정</Text>
-        <Text onPress={onPressDelete} style={styles.optionItem}>삭제</Text>
+        <TouchableOpacity onPress={onPressDone} style={styles.optionItem}> 
+          <Text style={styles.textAlignCenter}>완료</Text> 
+        </TouchableOpacity>
+        <TouchableOpacity  onPress={onPressEdit} 
+          style={
+            {...styles.optionItem, 
+              borderLeftColor: '#bbb', 
+              borderLeftWidth: 1,
+              borderRightColor: '#bbb',
+              borderRightWidth: 1,
+            }}> 
+          <Text style={styles.textAlignCenter}>수정</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onPressDelete} style={styles.optionItem}> 
+          <Text style={styles.textAlignCenter}>삭제</Text>
+          </TouchableOpacity>
      </View>}
     </TouchableOpacity>
     
