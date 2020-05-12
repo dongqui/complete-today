eaacb2475080198a225ebb4c0df5618f1b5b7cb7
Unknown <kimwinner90@gmail.com>
2020-05-12 18:25:08 +0900

    이미지 추가

--- src/components/TodoList/DoneItem.js
+++ src/components/TodoList/DoneItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Text, View, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 import styles from './TodoStyles';
 
@@ -22,6 +23,7 @@ export default function DoneItem({ todo }) {
   return (
     <TouchableOpacity onPress={press}>
       <View style={styles.itemContainer}>
+        <AntDesign name="smile-circle" size={24} color="black" />
         <Text style={styles.doneItem} >{todo.content}</Text>
       </View>
 
